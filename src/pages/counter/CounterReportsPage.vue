07ad1bf40594c74adc286e6c8524afc9cbbd32ed
Bozana Bokan <bozana.bokan@posteo.net>
2024-07-16 15:01:55 +0200

    src pages counter

---
+++ src/pages/counter/CounterReportsPage.vue
@@ -0,0 +1,17 @@
+<template>
+	<h1 class="text-default">Example page</h1>
+	<Panel>
+		<PanelSection>
+			<CounterReportsListPanel
+				v-bind="components.counterReportsListPanel"
+				@set="set"
+			/>
+		</PanelSection>
+	</Panel>
+</template>
+
+<script setup>
+import Panel from '@/components/Panel/Panel.vue';
+import PanelSection from '@/components/Panel/PanelSection.vue';
+import CounterReportsListPanel from '@/components/ListPanel/counter/CounterReportsListPanel.vue';
+</script>
