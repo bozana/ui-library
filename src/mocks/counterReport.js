d9b28d4b60e76d5c27cf9a08e9cf04aa100a44e0
Bozana Bokan <bozana.bokan@posteo.net>
2024-07-17 15:36:59 +0200

    storybook

---
+++ src/mocks/counterReport.js
@@ -0,0 +1,12 @@
+export default {
+	Report_ID: 'PR',
+	begin_date: '2022-02-03',
+	end_date: '2024-07-17',
+	customer_id: 0,
+	metric_type: {
+		0: 'Total_Item_Investigations',
+		1: 'Unique_Item_Investigations',
+		2: 'Total_Item_Requests',
+		3: 'Unique_Item_Requests',
+	},
+};
